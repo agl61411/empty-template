c089e12a93072120955f6d16a8744287dd2c327b
yi <614110832@qq.com>
2023-04-14 11:01:20 +0800

    terser

--- vue.config.js
+++ vue.config.js
@@ -99,7 +99,7 @@ module.exports = {
             filename: '[path].gz[query]', // 压缩后的文件名(保持原文件名，后缀加.gz)
             minRatio: 1, // 压缩率小于1才会压缩
             threshold: 10240, // 对超过10k的数据压缩
-            deleteOriginalAssets: true, // 是否删除未压缩的源文件，谨慎设置，如果希望提供非gzip的资源，可不设置或者设置为false（比如删除打包后的gz后还可以加载到原始资源文件）
+            deleteOriginalAssets: true // 是否删除未压缩的源文件，谨慎设置，如果希望提供非gzip的资源，可不设置或者设置为false（比如删除打包后的gz后还可以加载到原始资源文件）
           })).end()
           config
             .optimization.splitChunks({
@@ -127,6 +127,13 @@ module.exports = {
             })
           // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
           config.optimization.runtimeChunk('single')
+          config.optimization.minimizer('terser').tap((args) => {
+            args[0].parallel = 4
+            args[0].terserOptions.compress.warnings = true
+            args[0].terserOptions.compress.drop_debugger = true
+            args[0].terserOptions.compress.drop_console = true
+            return args
+          })
         }
       )
   }
